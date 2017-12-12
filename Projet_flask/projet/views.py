ba6a642bd252f6cf13d1be1b288eaf4c4de433ad
Arthur Fauvin <fauvin@info25-01.iut45.univ-orleans.fr>
2017-12-12 10:03:20 +0100

    debut css page connexion

--- Projet_flask/projet/views.py
+++ Projet_flask/projet/views.py
@@ -5,7 +5,4 @@ from flask import render_template
 
 @app.route("/")
 def home():
-	return render_template("connexion.html", title= "Premier template avec Flask")
-
-
-
+	return render_template("connexion.html", title= "Exerciseur MCD")
