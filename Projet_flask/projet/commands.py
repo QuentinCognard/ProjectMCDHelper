49b0ad2cdb7631435dd89da7259b1a1b3406fad5
Arthur Fauvin <fauvin@info25-09.iut45.univ-orleans.fr>
2018-01-12 11:56:18 +0100

    Modifs

--- Projet_flask/projet/commands.py
+++ Projet_flask/projet/commands.py
@@ -1,3 +1,16 @@
-from .app import manager,db
+from .app import manager, db
+import yaml
+from .models import *
+from hashlib import sha256
 
+@manager.command
+def syncdb():
+    db.create_all()
 
+@manager.command
+def newuser(login, password):
+    m = sha256()
+    m.update(password.encode())
+    u = User(prenom = None, nom = None, login=login, password = m.hexdigest())
+    db.session.add(u)
+    db.session.commit()
